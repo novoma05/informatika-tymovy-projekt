1c559bf3090c9882b99def612bba9133f518c49b
Matěj Novotný <119802846+novoma05@users.noreply.github.com>
2025-11-13 23:39:16 +0100

    enter to stop
    Refactor main function to run animation in a separate thread and allow user input to stop it.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include "draha.h"
+#include <thread>
 #include <iostream>
 
-using namespace std;
+int main()
+{
+    // Run animation on separate thread so main can continue or wait.
+    std::thread anim(bwl_koule, -1); // -1 = nekonecno
+    std::cout << "Press Enter to stop...\n";
+    std::cin.get();
+    std::cout << "Vystrel\n";
 
-int main() {
-    
+    anim.detach();
+
+    return 0;
 }
